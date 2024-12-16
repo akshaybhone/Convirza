0493d089f010d3b73d9d806cc5ffe27a8af78924
akshaybhone <akshaybhone09@gmail.com>
2024-12-16 15:01:53 +0530

    new code mvn commands 16/12

--- src/test/java/testbase/BaseTest.java
+++ src/test/java/testbase/BaseTest.java
@@ -8,6 +8,8 @@ import java.util.Properties;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -59,7 +61,7 @@ public class BaseTest {
 	
 	
 	// Use below code when individual class is to be Run
-
+/*
 	@BeforeClass
 	// @Parameters({ "os", "browser" })
 	public void setup() throws IOException {
@@ -77,8 +79,39 @@ public class BaseTest {
 		driver.get(p.getProperty("appBetaURL"));
 		//driver.get(p.getProperty("appProdURL"));
 		driver.manage().window().maximize();
-	}
+	}*/
 
+	// Use below code when individual class is to be Run with Runtime parameter from maven command prompt
+
+		@BeforeClass
+		// @Parameters({ "os", "browser" })
+		public void runTimesetup() throws IOException {
+
+			// loading properties file
+			FileReader file = new FileReader(".\\src\\test\\resources\\config.properties");
+			p = new Properties();
+			p.load(file);
+			String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") :p.getProperty("browser");
+			//String browserName = p.getProperty("browser");
+			if (browserName.equalsIgnoreCase("chrome")) {
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+			}else if (browserName.equalsIgnoreCase("firefox")) {
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+			}else if (browserName.equalsIgnoreCase("edge")) {
+				WebDriverManager.edgedriver().setup();
+				driver = new EdgeDriver();
+			}
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+			// driver.get("https://stag-5-cai.convirza.com/login");
+
+			driver.get(p.getProperty("appBetaURL"));
+			//driver.get(p.getProperty("appProdURL"));
+			driver.manage().window().maximize();
+		}
+	
+	
 	@AfterClass
 	public void teardown() {
 
